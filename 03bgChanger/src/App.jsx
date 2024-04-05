cee4522883a84e9700bdceb71ff4cdb768f47377
ericnygma <xenygma@gmail.com>
2024-04-05 06:44:15 -0400

    added input

--- 03bgChanger/src/App.jsx
+++ 03bgChanger/src/App.jsx
@@ -11,19 +11,22 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-500'>Bockground Changer with Vite</h1>
+      <h1 className='bg-gradient-to-br from-yellow-400 to-purple-600 font-bold text-center text-xl'>Bockground Changer with Vite & Tailwind</h1>
       <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
             <button
-            className='outline-none shadow-lg px-4 py-1 rounded-full text-black'
+            className='outline-none shadow-lg px-4 py-1 rounded-full text-white bg-red-600'
             onClick={() => changeColor('red')}>Red</button>
             <button
-            className='outline-none shadow-lg px-4 py-1 rounded-full text-black'
+            className='outline-none shadow-lg px-4 py-1 rounded-full text-white bg-blue-700'
             onClick={() => setColor('blue')}>Blue</button>
             <button
-            className='outline-none shadow-lg px-4 py-1 rounded-full text-black'
-            onClick={''}>test3</button>
+            className='outline-none shadow-lg px-4 py-1 rounded-full text-white bg-orange-500'
+            onClick={() => setColor('orange')}>Orange</button>
+            <input type="color" 
+            className='outline-none shadow-lg px-4 py-2 rounded-full bg-gradient-to-br from-yellow-400 to-purple-500' 
+            onChange={(e) => setColor(e.target.value)}/>
           </div>
         </div>
       </div>
